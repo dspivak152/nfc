023d0d5db1ebad3ca59c7be9fb36ba62c26e4849
Danny Spivak <dspivak152@gmail.com>
2019-11-12 22:01:54 +0200

    fix the new api requirment

--- src/app/models/roomAvailable.ts
+++ src/app/models/roomAvailable.ts
@@ -1,13 +1,13 @@
 export class RoomAvailable {
-    constructor(roomNumber: number, name: string, hotelId: number) {
+    constructor(roomNumber: number, name: string, hotelId: number/*, email: string*/) {
         this.roomNumber = roomNumber;
         this.name = name;
-        //this.email = email;
+        // this.email = email;
         this.hotelId = hotelId;
     }
 
     roomNumber: number;
     name: string;
-    //email: string;
+    // email: string;
     hotelId: number;
 }
